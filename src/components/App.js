53e50476f9cb239b877d2aa863918bcc07a3de3f
Swati Suman <104189142+swatisuman27@users.noreply.github.com>
2022-09-20 20:51:38 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -17,40 +17,38 @@ const characters = [
   {
     id: 3,
     name: "Yoru",
-    role: "Duelist",
-    abilities: ["GateCrash", "Fakeout", "Blind Side", "Dimensional Drift"],
-  },
-  {
-    id: 4,
-    name: "Reyna",
-    role: "Duelist",
-    abilities: ["Dismiss", "Leer", "Devour", "Empress"],
-  },
-  {
-    id: 5,
-    name: "Raze",
-    role: "Duelist",
-    abilities: ["Paint Shells", "Boom Bot", "BlastPack", "ShowStopper"],
-  },
-  {
-    id: 6,
-    name: "Neon",
-    role: "Duelist",
-    abilities: ["High Gear", "Fast Lane", "Relay Bolt", "Over Drive"],
-  },
-  {
-    id: 7,
-    name: "Sova",
-    role: "Initiator",
-    abilities: ["Recon Bolt", "Owl Drone", "Shock Bolt", "Hunter's Fury"],
-  },
-  {
-    id: 8,
-    name: "Fade",
-    role: "Initiator",
-    abilities: ["Haunt", "Prowler", "Seize", "NightFall"],
-  },
-  {
+    role: "Duelist", abilities: ["GateCrash", "Fakeout", "Blind Side", "Dimensional Drift"],
+},
+{
+  id: 4,
+  name: "Reyna",
+  role: "Duelist",
+  abilities: ["Dismiss", "Leer", "Devour", "Empress"],
+},
+{
+  id: 5,
+  name: "Raze",
+  role: "Duelist",
+  abilities: ["Paint Shells", "Boom Bot", "BlastPack", "ShowStopper"],
+},
+{
+  id: 6,
+  name: "Neon",
+  role: "Duelist",
+  abilities: ["High Gear", "Fast Lane", "Relay Bolt", "Over Drive"],
+},
+{
+  id: 7,
+  name: "Sova",
+  role: "Initiator",
+  abilities: ["Recon Bolt", "Owl Drone", "Shock Bolt", "Hunter's Fury"],
+},
+{
+  id: 8,
+  name: "Fade",
+  role: "Initiator",
+  abilities: ["Haunt", "Prowler", "Seize", "NightFall"],
+}, {
     id: 9,
     name: "Skye",
     role: "Initiator",
@@ -65,28 +63,26 @@ const characters = [
   {
     id: 11,
     name: "Breach",
-    role: "Initiator",
-    abilities: ["Fault Line", "AfterShock", "FlashPoint", "Rolling Thunder"],
-  },
-  {
-    id: 12,
-    name: "Omen",
-    role: "Controller",
-    abilities: ["Dark Cover", "Shrouded Step", "Paranoia", "From the Shadows"],
-  },
-  {
-    id: 13,
-    name: "Astra",
-    role: "Controller",
-    abilities: ["Nebula", "Gravity Well", "Nova Pulse", "Cosmic Divide"],
-  },
-  {
-    id: 14,
-    name: "Viper",
-    role: "Controller",
-    abilities: ["Toxic Screen", "Snake Bite", "Poison Cloud", "Viper's PIT"],
-  },
-  {
+    role: "Initiator",  abilities: ["Fault Line", "AfterShock", "FlashPoint", "Rolling Thunder"],
+},
+{
+  id: 12,
+  name: "Omen",
+  role: "Controller",
+  abilities: ["Dark Cover", "Shrouded Step", "Paranoia", "From the Shadows"],
+},
+{
+  id: 13,
+  name: "Astra",
+  role: "Controller",
+  abilities: ["Nebula", "Gravity Well", "Nova Pulse", "Cosmic Divide"],
+},
+{
+  id: 14,
+  name: "Viper",
+  role: "Controller",
+  abilities: ["Toxic Screen", "Snake Bite", "Poison Cloud", "Viper's PIT"],
+}, {
     id: 15,
     name: "Brimstone",
     role: "Controller",
@@ -117,42 +113,56 @@ const characters = [
     abilities: ["Spycam", "Trapwire", "Cyber Cage", "Nueral Theft"],
   },
 ];
-
 const App = () => {
   const [currChar, setCurrChar] = useState({
     name: "",
-    role: "",
-    abilities: [],
-    options: [],
+    role: "",abilities: [],
+    options: []
   });
 
   const [score, setScore] = useState(0);
 
-  const changeChar = () => {
-    
-  };
-
+  const changeChar = () => {};
+  changeChar();
   const scoreHandler = (e) => {
-   
+    if (e.target.innerText === currChar.name) {
+      setScore(score + 1);
+    } else {
+        setScore(score - 1);
+    }
   };
 
   useEffect(() => {
-   
-  });
+    let random = Math.floor(Math.random() * 15);
+    // setCurrChar(()=>{
+    //   return characters[random]
+    // })
+    setCurrChar({
+      name: characters[random].name,
+      role: characters[random].role,
+      abilities: characters[random].abilities,
+      options: [
+        characters[random + 1],
+        characters[random + 2],
+        characters[random + 3],
+        characters[random] ]
+    });
+  }, [score]);
+
   return (
     <div id="main">
       <div className="container">
         <h1 className="header">Guess the Character</h1>
         <div className="ques-area">
-          <div className="score" id='score'>Score: {score}</div>
+          <div className="score" id="score">
+            Score: {score}
+          </div>
           <h3>The character has the following abilities:</h3>
           <h4>Role: {currChar.role}</h4>
           {currChar.abilities.join()}
           <div className="options">
             {currChar.options.map((option) => (
-              <button   onClick={scoreHandler}>
-                {option.name}
-              </button>
+              <button onClick={scoreHandler}>{option.name}</button>
             ))}
           </div>
         </div>
